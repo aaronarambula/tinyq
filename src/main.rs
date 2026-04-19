9b9c48ef9fa001ecebcc04b4ef77d93bdd568d0f
aaronarambula <aaronarambula2007@icloud.com>
2026-04-18 20:42:32 -0700

    feat: del command utilizing tombstone

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,14 @@ fn main() {
         for line in reader.lines() {
             let line = line.expect("Failed to read line");
             let parts: Vec<&str> = line.split_whitespace().collect();
-            if parts.len() == 2 {
-                db.insert(parts[0].to_string(), parts[1].to_string());
+            match parts.as_slice() {
+                ["D", key] => {
+                    db.remove(*key);
+                }
+                ["S", key, value] => {
+                    db.insert(key.to_string(), value.to_string());
+                }
+                _ => {}
             }
         }
     }
@@ -38,6 +44,10 @@ fn main() {
 
         match words[0] {
             "set" => {
+                if words.len() != 3 {
+                    println!("Usage: set <key> <value>");
+                    continue;
+                }
                 let key = words[1].to_string();
                 let value = words[2].to_string();
                 let mut file = OpenOptions::new()
@@ -46,15 +56,33 @@ fn main() {
                     .open("db.txt")
                     .unwrap();
 
-                writeln!(file, "{} {}", key, value).unwrap();
+                writeln!(file, "S {} {}", key, value).unwrap();
                 db.insert(key, value);
             }
             "get" => {
+                if words.len() != 2 {
+                    println!("Usage: get <key>");
+                    continue;
+                }
                 let key = words[1];
                 match db.get(key) {
                     Some(value) => println!("{}", value),
                     None => println!("Key not found"),
                 }
+            }
+            "del" => {
+                if words.len() != 2 {
+                    println!("Usage: del <key>");
+                    continue;
+                }
+                let key = words[1].to_string();
+                let mut file = OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open("db.txt")
+                    .unwrap();
+                writeln!(file, "D {}", key).unwrap();
+                db.remove(&key);
             }
 
             "exit" => break,
